ddf92d62a7830f6e641ac2f7d5231c59cc451101
Tobias Waslowski <contact@twaslowski.com>
2026-01-16 16:05:22 +0200

    feat: add rest interfaces todo creation and completion

--- golang/main.go
+++ golang/main.go
@@ -7,19 +7,47 @@ import (
 )
 
 func main() {
-	r := setupRouter()
+	todoManager := NewTodoManager()
+	todoService := TodoService{todoManager}
+
+	r := todoService.setupRouter()
 	err := r.Run()
 	if err != nil {
 		fmt.Printf("Got error when starting webserver: %s", err)
 	}
 }
 
-func setupRouter() *gin.Engine {
+func (t TodoService) setupRouter() *gin.Engine {
 	r := gin.Default()
 	r.GET("/health", func(context *gin.Context) {
 		context.JSON(200, gin.H{
 			"message": "up",
 		})
 	})
+	r.POST("/todo", t.createTodo)
+	r.GET("/todo", t.createTodo)
+	r.POST("/todo/%d/complete", t.completeTodo)
 	return r
+}
+
+func (t TodoService) createTodo(context *gin.Context) {
+	var newTodo Todo
+	if err := context.BindJSON(&newTodo); err != nil {
+		context.JSON(400, gin.H{"error": err.Error()})
+		return
+	}
+	t.TodoManager.Add(newTodo)
+
+	context.JSON(201, gin.H{"todo": newTodo})
+}
+
+func (t TodoService) findTodo(context *gin.Context) {
+	var todo Todo
+
+	context.JSON(201, gin.H{"todo": todo})
+}
+
+func (t TodoService) completeTodo(context *gin.Context) {
+	var todo Todo
+	context.JSON(201, gin.H{"todo": todo})
 }
--- golang/todo.go
+++ golang/todo.go
@@ -3,14 +3,23 @@ package main
 import "time"
 
 type Todo struct {
-	Id        int32
-	Title     string
-	Completed bool
-	DueDate   time.Time
+	Id        int32     `json:"id"`
+	Title     string    `json:"title"`
+	Completed bool      `json:"completed"`
+	DueDate   time.Time `json:"dueDate"`
 }
 
+// todo refactor to type alias for map<int, Todo>
 type TodoManager struct {
 	Todos []Todo
+}
+
+func NewTodoManager() TodoManager {
+	return TodoManager{[]Todo{}}
+}
+
+type TodoService struct {
+	TodoManager TodoManager
 }
 
 func (t *TodoManager) Add(todo Todo) {
